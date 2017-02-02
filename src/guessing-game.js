1587276d41464b03c5e6514891d35749916057c5
Aliaksei Varanovich <aleks.voronovich@gmail.com>
2017-02-02 15:58:31 +0300

    Guessing game: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+		this.start = null,
+		this.stop = null,
+		this.center = null;
+	}
 
     setRange(min, max) {
-
+		this.start = min;
+		this.stop = max;
     }
 
     guess() {
-
+		this.center = Math.round((this.start + this.stop)/2);
+		return this.center;
     }
 
     lower() {
-
+		this.stop = this.center;
     }
 
     greater() {
-
+		this.start = this.center;
     }
 }
 
